36c49430ab7724d818bb3771506147cfda72af07
李石 <925711777@>
2018-01-25 00:43:03 +0800

    config

---
+++ AndroidStudioProjects/Project/myapplication/src/main/java/com/example/myapplication/Config.java
@@ -0,0 +1,12 @@
+package com.example.myapplication;
+
+/**
+ * Created by lishi on 2018/1/19.
+ * 作用
+ */
+
+public class Config {
+    public static final String HOST = "http://www.zhihuiweijian.com:8080/help";
+    public static final String API =":8080/help/findHospitalsByAreaId?area_id=110101";
+    public static final String HOST_API = HOST + API;
+}
